d430b9ad24e9fb51814932f64438685d9a4caf86
Shubham Srivastava <shubhamsri0807@gmail.com>
2026-01-18 13:05:38 +0530

    Backend changes

--- backend/test.js
+++ backend/test.js
@@ -16,3 +16,31 @@ for(let i =0; i<arr.length; i++){
 }
 
 console.log(min);
+
+
+/// sorting via loop decendng
+
+for(let i=0; i<arr.length; i++){
+    for(let j=0; j<arr.length-1-i; j++){
+        if(arr[j]<arr[j+1]){
+            let temp=arr[j]
+            arr[j]=arr[j+1]
+            arr[j+1]=temp
+        }
+    }
+}
+
+console.log(arr);
+
+/// sorting via loop accending
+for(let i=0; i<arr.length; i++){
+    for(let j=0; j<arr.length-1-i; j++){
+        if(arr[j]>arr[j+1]){
+            let temp=arr[j]
+            arr[j]=arr[j+1]
+            arr[j+1]=temp
+        }
+    }
+}
+
+console.log(arr);
