ae1e5b999f8ace7067f5a26ce479678e19bce39c
Shubham Srivastava <shubhamsri0807@gmail.com>
2026-02-02 21:17:40 +0530

    sideeffctes of useeffcect

--- frontend/day_1/src/App.jsx
+++ frontend/day_1/src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-let name="Shubham Srivastava"
+  const [count, setCount]= useState(0);
+  const increment =()=>{
+    setCount(count+1)
+  }
+    const decrement =()=>{
+      if(count>=1){
+        setCount(count-1)
+      }
+
+  }
+
+  useEffect(()=>{
+    console.log("count change : " + count)
+  },[count])
+
+
   return (
     <>
       <div>
-        {name}
+        Counter
       </div>
+      <div>
+        {count}
+        <button onClick={increment}>increment</button>
+        <button onClick={decrement}>decrement</button>
+      </div>
+      
     </>
   )
 }
