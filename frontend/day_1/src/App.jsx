2bef39a316c89f6106cf87122f45e43359c9c4fd
Shubham Srivastava <shubhamsri0807@gmail.com>
2026-02-04 22:28:46 +0530

    fixed cors error still geeting soem error

--- frontend/day_1/src/App.jsx
+++ frontend/day_1/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import GetUserList from './basicApi/GetUserList'
 import Counter from './counter/Counter'
 import Login from './loginForm/Login'
 
 function App() {
   return <>
     {/* <Counter/> */}
-    <Login/>
+    {/* <Login/> */}
+    <GetUserList/>
   </>
 }
 
